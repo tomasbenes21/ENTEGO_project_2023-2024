8398ef769c79b4c3db9c2479c88aac2c48a00ca2
tomasbenes21 <149105046+tomasbenes21@users.noreply.github.com>
2024-01-18 13:55:23 +0100

    Create Q1 final.sql

---
+++ Progress/Question 1/Q1 final.sql
@@ -0,0 +1,27 @@
+
+SELECT
+	table1.industry_name,
+	table1.year AS previous_year,
+	avg (table1.payroll_value) AS avg_payroll_value_previous_year,
+	table2.year,
+	avg (table2.payroll_value) AS avg_payroll_value,
+	CASE
+	WHEN avg (table1.payroll_value) < avg (table2.payroll_value) THEN '↑'
+	ELSE '↓'
+END AS payroll_growth 
+FROM (
+	SELECT *
+	FROM t_tomas_benes_project_sql_primary_final ttbpspft
+	WHERE value_name = 'Průměrná hrubá mzda na zaměstnance'
+		AND industry_name IS NOT NULL
+ ) table1
+ JOIN (
+	SELECT *
+	FROM t_tomas_benes_project_sql_primary_final ttbpspft
+	WHERE value_name = 'Průměrná hrubá mzda na zaměstnance'
+		AND industry_name IS NOT NULL
+ ) table2
+	 ON table1.industry_name = table2.industry_name
+	 AND table1.year = table2.YEAR -1
+GROUP BY 	table1.industry_name, table1.year, table2.YEAR
+HAVING payroll_growth = '↓'
